5a2975f423a268d0dca285c761b006a6f4bd3c5e
Daniel Qenani <danielqenani@Daniels-MBP-36.lan>
2023-09-18 20:18:37 -0400

    optimized ShadowAssist.py

--- src/ShadowAssist.py
+++ src/ShadowAssist.py
@@ -1,12 +1,28 @@
 from qiskit.primitives import Estimator
 from qiskit import QuantumCircuit, transpile, Aer
-from qiskit.providers.aer import QasmSimulator
+from qiskit.providers.aer import QasmSimulator 
 from qiskit.providers.fake_provider import FakeMontreal
 from qiskit.quantum_info.operators import Operator, Pauli
 import numpy as np
 
+#global variables to be used in the parallel processed function below
 default_device = Aer.get_backend('aer_simulator')
 map_result = {"0": 1, "1": -1}
+
+
+ '''
+     This function acts as a helper function for the parallelization of calc_shadow. It takes quantum circuit and unitaries to
+     rotate the state, performs the measurement, and returns a list of the results.
+     
+     Args:
+         indices (List of int): contains a list of the enumerated unitaries to rotate the state (Pauli gates: X, Y, Z)
+         circ (QuantumCircuit): the quantum circuit which contains the quantum state to rotate and measure
+         num_qubits(int): the size of the quantum system
+         
+     
+     Returns:
+         vals(List of int): returns a list of the measurement results                      
+ '''
 
 def shadow_assist(indices, circ, num_qubits, device = default_device):
     vals = np.zeros(num_qubits)
