c61611b36099fc03c9fdaaa97a3860dfcd9b7798
Daniel Qenani <danielqenani@Daniels-MBP-36.lan>
2023-10-24 19:45:15 -0400

    update PennylaneClassicalShadows.py

--- src/PennylaneClassicalShadows.py
+++ src/PennylaneClassicalShadows.py
@@ -55,3 +55,55 @@ def shadow_state_reconstruction(shadow):
 
 def operator_norm(R):
     return np.sqrt(np.abs(np.trace(R.conjugate().transpose()@R)))
+
+def estimate_shadow_obervable(shadow, observable, k=10):
+    shadow_size, num_qubits = shadow[0].shape
+
+    # convert Pennylane observables to indices
+    map_name_to_int = {"PauliX": 0, "PauliY": 1, "PauliZ": 2}
+    if isinstance(observable, (qml.PauliX, qml.PauliY, qml.PauliZ)):
+        target_obs, target_locs = np.array(
+            [map_name_to_int[observable.name]]
+        ), np.array([observable.wires[0]])
+    else:
+        target_obs, target_locs = np.array(
+            [map_name_to_int[o.name] for o in observable.obs]
+        ), np.array([o.wires[0] for o in observable.obs])
+
+    # classical values
+    b_lists, obs_lists = shadow
+    means = []
+
+    # loop over the splits of the shadow:
+    for i in range(0, shadow_size, shadow_size // k):
+
+        # assign the splits temporarily
+        b_lists_k, obs_lists_k = (
+            b_lists[i: i + shadow_size // k],
+            obs_lists[i: i + shadow_size // k],
+        )
+
+        # find the exact matches for the observable of interest at the specified locations
+        indices = np.all(obs_lists_k[:, target_locs] == target_obs, axis=1)
+
+        # catch the edge case where there is no match in the chunk
+        if sum(indices) > 0:
+            # take the product and sum
+            product = np.prod(b_lists_k[indices][:, target_locs], axis=1)
+            means.append(np.sum(product) / sum(indices))
+        else:
+            means.append(0)
+
+    return np.median(means)
+
+def shadow_bound(error, observables, failure_rate=0.01):
+    M = len(observables)
+    K = 2 * np.log(2 * M / failure_rate)
+    shadow_norm = (
+        lambda op: np.linalg.norm(
+            op - np.trace(op) / 2 ** int(np.log2(op.shape[0])), ord=np.inf
+        )
+        ** 2
+    )
+    N = 34 * max(shadow_norm(o) for o in observables) / error ** 2
+    return int(np.ceil(N * K)), int(K)
